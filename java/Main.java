d0f974a1e05f4407b7dbbdd28378e46b723a473f
dsipaint <57596940+dsipaint@users.noreply.github.com>
2020-07-24 01:59:52 +0100

    Began implementing logging with SLF4J

--- java/Main.java
+++ java/Main.java
@@ -1,18 +1,36 @@
 import java.io.IOException;
 
+import javax.security.auth.login.LoginException;
+
 import org.json.simple.DeserializationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
 public class Main
 {
+	static JDA jda;
+	static final Logger logger = LoggerFactory.getLogger("AMGN"); //logger
+	
 	public static void main(String[] args)
 	{
 		//SETUP
-		
-		System.out.println("Commencing setup..."); //TODO: change to logging
+		logger.info("Commencing setup...");
+		try
+		{
+			jda = new JDABuilder(AccountType.BOT).setToken("NjQyODM5OTg5NjQxNjc0NzUy.Xxoubw.jBwpkEl41CzDkO2kVen1Q_NqH9Q").build();
+		}
+		catch (LoginException e1)
+		{
+			e1.printStackTrace();
+		}
 		
 		try
 		{
-			System.out.println("Reading data..."); //TODO: change to logging
+			logger.info("Reading data...");
 			GuildNetwork.guild_data = Data.readGuildData("guilds.json"); //read guild data from guilds.json
 		}
 		catch (IOException | DeserializationException e)
@@ -20,7 +38,7 @@ public class Main
 			e.printStackTrace();
 		}
 		
-		System.out.println("Finished setup."); //TODO: change to logging
+		logger.info("Finished setup.");
 		//END SETUP
 		
 		
@@ -29,13 +47,15 @@ public class Main
 		//TEST/DEBUG
 		GuildNetwork.guild_data.forEach((id, guild) ->
 		{
-			System.out.println("Guild id: " + id);
-			System.out.println("Prefix: " + guild.getPrefix());
-			System.out.println("Modlogs: " + guild.getModlogs());
-			System.out.println("Modrole: " + guild.getModrole());
+			logger.debug("Guild id: " + id);
+			logger.debug("Prefix: " + guild.getPrefix());
+			logger.debug("Modlogs: " + guild.getModlogs());
+			logger.debug("Modrole: " + guild.getModrole());
 			
 			System.out.println();
 		});
+		
+		jda.shutdownNow();
 		//END TEST/DEBUG
 	}
 }
