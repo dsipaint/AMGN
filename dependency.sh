82c29478caaa8689d48e31c544e01222e8abd6a6
dsipaint <finlay.woolfson@gmail.com>
2022-04-13 16:10:55 +0100

    update dependency.sh

--- dependency.sh
+++ dependency.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # RUN THIS TO ADD  AMGN AS A DEPENDENCY
-bash build.sh
+maven package
 mvn install:install-file -Dfile=target/AMGN-alpha-1.2.0-jar-with-dependencies.jar -DgroupId=com.github.dsipaint -DartifactId=AMGN -Dversion=alpha-1.2.0 -Dpackaging=jar -DgeneratePom=true
