426982c0be1cfcbab95e2a44dae6998e93d2b7a2
dsipaint <finlay.woolfson@gmail.com>
2022-10-26 18:16:14 +0100

    add basic template for bot info

---
+++ src/main/java/com/github/dsipaint/AMGN/main/web/WebpanelController.java
@@ -0,0 +1,34 @@
+package com.github.dsipaint.AMGN.main.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+import com.github.dsipaint.AMGN.main.AMGN;
+
+@Controller
+public class WebpanelController
+{
+    @RequestMapping("/webpanel")
+    public String getCustomWebpanel(Model model)
+    {
+        model.addAttribute("botname", AMGN.bot.getSelfUser().getName());
+        model.addAttribute("botpfp", AMGN.bot.getSelfUser().getAvatarUrl());
+        return "homepage";
+    }
+
+    @Bean
+    public FileTemplateResolver secondaryTemplateResolver()
+    {
+        FileTemplateResolver templateResolver = new FileTemplateResolver();
+        templateResolver.setPrefix("./web/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+                
+        return templateResolver;
+    }
+}
