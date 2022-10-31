bc0477787f9e7b46aac7059a9761e739ee854de7
dsipaint <finlay.woolfson@gmail.com>
2022-10-31 16:34:33 +0000

    fix login process on frontend

--- src/main/resources/web/cleanup.js
+++ src/main/resources/web/cleanup.js
@@ -1,7 +1,7 @@
 function toggleLoginVisibility()
 {
     //look through cookies
-    var cookies = document.cookie.split(";")
+    var cookies = document.cookie.split("; ");
     for(var i = 0; i < cookies.length; i++)
     {
         var cookie = cookies[i].split("=");
@@ -10,16 +10,25 @@ function toggleLoginVisibility()
             for(var j = 0; j < cookies.length; j++)
             {
                 var cookie2 = cookies[j].split("=");
+                console.log(cookie2);
                 if(cookie2[0] == "discord_username")
                     $("#username").text(cookie2[1]);
                 else if(cookie2[0] == "discord_pfp")
                     $("#userpfp").attr("src", cookie2[1]);
             }
-            
+
             $(".token").show();
             return;
         }
     }
 
     $(".notoken").show();
+}
+
+function clearcookies()
+{
+    document.cookie = "discord_username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+    document.cookie = "discord_pfp=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+    document.cookie = "discord_token=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+    location.reload();
 }
