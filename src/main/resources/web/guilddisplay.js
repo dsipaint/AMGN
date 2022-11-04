7836c9510f31a42237bc17038bbfd6114a2bbe95
dsipaint <finlay.woolfson@gmail.com>
2022-11-04 16:28:14 +0000

    hide or show guild when appropriate places are clicked

--- src/main/resources/web/guilddisplay.js
+++ src/main/resources/web/guilddisplay.js
@@ -1,3 +1,13 @@
+function hideGuilds()
+{
+    $(".guild").hide();
+}
+
+function showGuilds()
+{
+    $(".guild").show();
+}
+
 class GuildList extends React.Component
 {
     constructor(props)
@@ -7,35 +17,37 @@ class GuildList extends React.Component
         //     console.log(data);
         //     //store guild data in array
         // });
-
-        //should just say "show guilds" until clicked
-
-        //keep the currently selected guild in state, store null for no guild selected
-        //in render function choose how to render based on this state property
-
-
-
-        //when clicked, display all guilds
-        //when a guild is clicked, display current guilds
-
-        //use an onclick method that will use jquery to show or hide .guild
-
-
         //temporary for testing
         this.state = {
             guilds: [{"id":"634667687233978388","name":"New Tester","picture":"https://cdn.discordapp.com/icons/634667687233978388/41a5b3467bfd8992a5e1f487d87822ad.png"},{"id":"700489766642253828","name":"Unnamed Project Dev Server","picture":null},{"id":"614260951167795204","name":"Safe Haven","picture":"https://cdn.discordapp.com/icons/614260951167795204/f95daf2df262f87aba0e64f197f28396.png"}],
-            selectedguild: null
+            // selectedguild: {"id":"634667687233978388","name":"New Tester","picture":"https://cdn.discordapp.com/icons/634667687233978388/41a5b3467bfd8992a5e1f487d87822ad.png"}
         }
+
+        this.selectGuild = this.selectGuild.bind(this);
+    }
+
+    selectGuild(id)
+    {
+        for(var i = 0; i < this.state.guilds.length; i++){
+            if(this.state.guilds[i].id == id)
+            {
+                this.setState({
+                    selectedguild: this.state.guilds[i]
+                });
+
+                break;
+            }
+        }
+        hideGuilds();
     }
 
     render()
     {
-
         return(
             <div>
-                Select Guild
+                <div id="selectedguild" onClick={showGuilds}>{this.state.selectedguild == null ? "Select Guild" : <span><img src={this.state.selectedguild.picture} width="30" height="30"></img><div>{this.state.selectedguild.name}</div></span>}</div>
                 {this.state.guilds.map(({picture, name}) =>(
-                    <div class="guild"><img src={picture} width="30" height="30"></img><div>{name}</div></div>
+                    <div class="guild" onClick={this.selectGuild}><img src={picture} width="30" height="30"></img><div>{name}</div></div>
                 ))}
             </div>
         );
