2389331e64abf90271390313f9e1b7f932c89460
dsipaint <finlay.woolfson@gmail.com>
2022-11-07 12:18:11 +0000

    fix plugin design for react

--- src/main/resources/web/plugindisplay.js
+++ src/main/resources/web/plugindisplay.js
@@ -9,20 +9,36 @@ class PluginConfig extends React.Component
     {
         return(
             <div>
-                <div id="plugintitle">
-                    <img src="" alt="plugin image" width="70" height="70"/>
-                    <h1>pluginname</h1>
+                <div id="pluginlist">
+                    <div class="plugin" onclick="">
+                        Plugin name 1
+                    </div>
+                    <div class="plugin" onclick="">
+                        Plugin name 2
+                    </div>
+                    <div class="plugin" onclick="">
+                        Plugin name 3
+                    </div>
+                    <div class="plugin" onclick="">
+                        Plugin name 4
+                    </div>
                 </div>
-                <p>Plugindescription</p>
-                <div id="options">
-                    options go here
+                <div id="pluginsettings">
+                    <div id="plugintitle">
+                        <img src="" alt="plugin image" width="70" height="70"/>
+                        <h1>pluginname</h1>
+                    </div>
+                    <p>Plugindescription</p>
+                    <div id="options">
+                        options go here
+                    </div>
+                    <div id="savesettings"  onclick="">Save Settings</div>
                 </div>
-                <div id="savesettings"  onclick="">Save Settings</div>
             </div>
         );
     }
 }
 
-const domContainer = document.querySelector('#maincontent');
+const domContainer = document.querySelector('#content');
 const root = ReactDOM.createRoot(domContainer);
 root.render(<PluginConfig />);
