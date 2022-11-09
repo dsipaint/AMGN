763fbe792526221d32c53c878b22934b6e9b78b3
dsipaint <finlay.woolfson@gmail.com>
2022-11-09 11:25:26 +0000

    make guild selector use guild data to display

--- src/main/resources/web/plugindisplay.js
+++ src/main/resources/web/plugindisplay.js
@@ -1,18 +1,15 @@
-function sayhi()
-{
-    console.log("hi!!!");
-}
-
 class PluginConfig extends React.Component
 {
     constructor(props)
     {
         super(props);
         this.state = {
-            plugins: []
+            plugins: [],
+            selectedplugin: {}
         }
 
         this.updatePluginNames = this.updatePluginNames.bind(this);
+        this.selectPlugin = this.selectPlugin.bind(this);
     }
 
     async componentDidMount()
@@ -27,23 +24,30 @@ class PluginConfig extends React.Component
         });
     }
 
+    selectPlugin(plugin)
+    {
+        this.setState({
+            selectedplugin: plugin
+        });
+    }
+
     render()
     {
         return(
             <div>
                 <div id="pluginlist">
-                    {this.state.plugins.map(({name}) =>(
-                        <div class="plugin" onClick={sayhi}>
+                    {this.state.plugins.map(({name, picture, description, version}) =>(
+                        <div class="plugin" onClick={() => {this.selectPlugin({name, picture, description, version})}}>
                             {name}
                         </div>
                     ))}
                 </div>
                 <div id="pluginsettings">
                     <div id="plugintitle">
-                        <img src="" alt="plugin image" width="70" height="70"/>
-                        <h1>pluginname</h1>
+                        <img src={this.state.selectedplugin.picture} alt="plugin image" width="70" height="70"/>
+                        <h1>{this.state.selectedplugin.name + " " + this.state.selectedplugin.version}</h1>
                     </div>
-                    <p>Plugindescription</p>
+                    <p>{this.state.selectedplugin.description}</p>
                     <div id="options">
                         options go here
                     </div>
