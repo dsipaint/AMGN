b268bf0fe95b745fcbf07b0addc9a4eff3fb7ed1
dsipaint <finlay.woolfson@gmail.com>
2022-11-09 12:19:31 +0000

    add author to plugin display

--- src/main/resources/web/plugindisplay.js
+++ src/main/resources/web/plugindisplay.js
@@ -36,19 +36,32 @@ class PluginConfig extends React.Component
         return(
             <div>
                 <div id="pluginlist">
-                    {this.state.plugins.map(({name, picture, description, version}) =>(
-                        <div class="plugin" onClick={() => {this.selectPlugin({name, picture, description, version})}}>
+                    <div class="plugin" onClick={() => {this.selectPlugin({})}}>
+                        Network Settings
+                    </div>
+                    {this.state.plugins.map(({name, picture, description, version, author}) =>(
+                        <div class="plugin" onClick={() => {this.selectPlugin({name, picture, description, version, author})}}>
                             {name}
                         </div>
                     ))}
                 </div>
 
                 {
-                    this.state.selectedplugin.name === undefined ? "No plugin selected!" :
+                    this.state.selectedplugin.name === undefined ? 
+                    
+                    <div id="plugintitle">
+                        <h1>Edit Network Settings</h1>
+                    </div>
+
+                    :
+
                     <div id="pluginsettings">  
                         <div id="plugintitle">
                             <img src={this.state.selectedplugin.picture} alt="plugin image" width="70" height="70"/>
-                            <h1>{this.state.selectedplugin.name + " " + this.state.selectedplugin.version}</h1>
+                            <div id="plugintitletxt">
+                                <h1>{this.state.selectedplugin.name + " " + this.state.selectedplugin.version}</h1>
+                                <em>{"by " + this.state.selectedplugin.author}</em>
+                            </div>
                         </div>
                         <p>{this.state.selectedplugin.description}</p>
                         <div id="options">
