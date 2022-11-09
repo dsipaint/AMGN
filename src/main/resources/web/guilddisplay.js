fae453878b6dabb8ff652bb35337673dcc56a8e6
dsipaint <finlay.woolfson@gmail.com>
2022-11-09 11:15:24 +0000

    fix guild selector showing proper selection

--- src/main/resources/web/guilddisplay.js
+++ src/main/resources/web/guilddisplay.js
@@ -53,7 +53,7 @@ class GuildList extends React.Component
     {
         return(
             <div>
-                { <div id="selectedguild" onClick={toggleGuilds}>{this.state.selectedguild ? "Select Guild" : <div><img id="selectedguildimg" src={this.state.selectedguild.picture} width="30" height="30"></img><div>{this.state.selectedguild.name}</div></div>}</div> }
+                { <div id="selectedguild" onClick={toggleGuilds}>{this.state.selectedguild.name === undefined ? "Select Guild" : <div><img id="selectedguildimg" src={this.state.selectedguild.picture} width="30" height="30"></img><div>{this.state.selectedguild.name}</div></div>}</div> }
                 {this.state.guilds.map(({picture, name, id}) =>(
                     <div class="guild" onClick={() => this.selectGuild(id)}><img src={picture} width="30" height="30"></img><div>{name}</div></div>
                 ))}
