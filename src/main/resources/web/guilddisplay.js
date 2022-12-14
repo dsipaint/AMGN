ce234ac042b94ff01a89cc3444f62d952a43750b
dsipaint <finlay.woolfson@gmail.com>
2022-12-14 20:16:13 +0000

    network settings only show for global settings, make global settings default-loaded option

--- src/main/resources/web/guilddisplay.js
+++ src/main/resources/web/guilddisplay.js
@@ -15,7 +15,10 @@ class GuildList extends React.Component
         super(props);
         this.state = {
             guilds: [],
-            selectedguild: {}
+            selectedguild: {
+                name: "Global Settings",
+                id: "global"
+            }
         }
         
         this.selectGuild = this.selectGuild.bind(this);
@@ -65,7 +68,14 @@ class GuildList extends React.Component
     {
         return(
             <div>
-                <div id="selectedguild" onClick={toggleGuilds}>{this.state.selectedguild.name === undefined ? "Select Guild" : <div><img id="selectedguildimg" src={this.state.selectedguild.picture} width="30" height="30"></img><div>{this.state.selectedguild.name}<span class="nodisplay">{this.state.selectedguild.id}</span></div></div>}</div>
+                <div id="selectedguild" onClick={toggleGuilds}>
+                    <div>
+                        <img id="selectedguildimg" src={this.state.selectedguild.picture} width="30" height="30"></img>
+                        <div>
+                            {this.state.selectedguild.name}<span class="nodisplay">{this.state.selectedguild.id}</span>
+                        </div>
+                    </div>
+                </div>
                 <div class="guild" onClick={() => this.selectGuild("global")}>Global Settings</div>
                 {this.state.guilds.map(({picture, name, id}) =>(
                     <div class="guild" onClick={() => this.selectGuild(id)}><img src={picture} width="30" height="30"></img><div>{name}</div></div>
